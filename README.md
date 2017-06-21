ab269d18991e82afae0f1b1cec1ac332223c9296
Travis <tjlemperle@gmail.com>
2017-06-20 19:56:40 -0600

    first commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# Learning
+First try on creating an webpage while using javascript
+# High Scores
+Select from Joust, qix, space invaders, asteroids, breakout
+Enter name
+Enter score
+Submit score to list
+sort highest to lowest
---
+++ app.js
@@ -0,0 +1,45 @@
+
+var highScores =[
+    {
+        score: 100,
+        name: "Ashley"
+        
+    }, 
+    {
+        score: 99,
+        name: "Jacob"
+
+    }
+];
+             
+        function myFunction() {
+                var nameInput = document.getElementById("nameInput").value
+                var scoreInput = document.getElementById("scoreInput").value
+                if(!Number(scoreInput)){
+                    alert("WRONG");
+                }
+                highScores.push({name: nameInput, score: scoreInput});
+                
+                highScores.sort(function(a, b){return b.score + a.score});
+
+                console.log(highScores);
+
+                
+
+                highScores.forEach(function(score, i) {
+
+                var table = document.getElementById("highScoresTable");
+                var row = table.insertRow(0);
+                var cell1 = row.insertCell(0);
+                var cell2 = row.insertCell(1);
+                var cell3 = row.insertCell(2);
+                cell1.innerHTML = i;
+                cell2.innerHTML = score.name;
+                cell3.innerHTML = score.score;
+
+                })
+               
+                
+                
+        }
+        
